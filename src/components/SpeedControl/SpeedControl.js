f5bae7d2757dfdeeecc4f5feaa4f916c3bb90145
Isaac Morerod <isaacmorerod@isaacsMacBookPro.local>
2019-05-20 13:35:17 -0500

    SPEED CONTROL buttons and SPEED working correctly

--- src/components/SpeedControl/SpeedControl.js
+++ src/components/SpeedControl/SpeedControl.js
@@ -1,20 +1,54 @@
 import React, { Component } from 'react';
+import mapReduxStateToProps from '../../modules/mapReduxStateToProps';
+import { connect } from 'react-redux';
+
 // THIS COMPONENT IS OUR INTERFACE FOR SPEED
 // YOU SHOULD DISPLAY THE CURRENT SPEED
 // BUTTONS SHOULD INCREASE OR DECREASE SPEED, RESPECTIVELY
 
 class SpeedControl extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      speed: '',
+    }
+  }
+
+  changeSpeed = (event) => {
+    const inputValue = event.target.value;
+    console.log(inputValue);
+    this.setState({
+      speed: inputValue,
+    })
+  }
+
+  clickIncrease = (event) => {
+    this.props.dispatch({
+      type: 'SPEED_INCREASE',
+    })
+    console.log('INCREASE FIRING');
+
+  }
+
+  clickDecrease = (event) => {
+    this.props.dispatch({
+      type: 'SPEED_DECREASE',
+    })
+    console.log('DECREASE FIRING');
+  }
+
   render() {
     return (
       <div>
         <h2>Speed Control</h2>
 
-        <button>Increase Speed</button>
-        <p>SPEED: GOES HERE</p>
-        <button>Decrease Speed</button>
+        <button onClick={this.clickIncrease}>Increase Speed</button>
+        <p>SPEED:{this.props.reduxState.speedReducer}</p>
+        <button onClick={this.clickDecrease}>Decrease Speed</button>
       </div>
     )
   }
 }
 
-export default SpeedControl;
+export default connect(mapReduxStateToProps)(SpeedControl);
--- src/index.js
+++ src/index.js
@@ -2,17 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 import logger from 'redux-logger';
 
 // put your reducers here!
-
+const speedReducer = (state = 0, action) => {
+  if (action.type === 'SPEED_DECREASE') {
+      const newState = state - 1;
+      return newState;
+  } else if (action.type === 'SPEED_INCREASE'){
+    const newState = state + 1;
+    return newState;
+  }
+  return state;
+}
 
 // be sure to combine your reducers!
 const storeInstance = createStore(
-  // reducers,
+  combineReducers({
+  speedReducer
+}),
   applyMiddleware(logger)
-)
+);
 
 ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
